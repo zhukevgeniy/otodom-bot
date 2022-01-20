afc92635fc20a1b6ba0992e71c53d94908752822
Jaŭhien Žuk <yauheni.zhuk@pandadoc.com>
2022-01-20 01:14:24 +0100

    OT-003: [feature] - Add sentry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TelegrafModule } from 'nestjs-telegraf';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
-import { sessionMiddleware } from './middlewares/session';
+import { SentryModule } from '@ntegral/nestjs-sentry';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OtodomModule } from './otodom/otodom.module';
+import { sessionMiddleware } from './middlewares/session';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    SentryModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dsn: configService.get<string>('SENTRY_DSN'),
+        environment: process.env.NODE_ENV,
+      }),
+      inject: [ConfigService],
+    }),
     TelegrafModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { SentryService } from '@ntegral/nestjs-sentry';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: false });
+
+  app.enableShutdownHooks();
+
+  app.useLogger(SentryService.SentryServiceInstance());
 
   await app.listen(process.env.PORT || 8080);
 }
